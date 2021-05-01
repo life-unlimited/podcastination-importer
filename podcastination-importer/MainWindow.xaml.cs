44a9a257e43671dde18398fac9a91f51d5f1f54a
Nils Bezjak <6823416-nils_bezjak@users.noreply.gitlab.com>
2021-05-01 20:47:03 +0200

    feature: added function on button click, that creates the json Object

--- podcastination-importer/MainWindow.xaml.cs
+++ podcastination-importer/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace podcastination_importer
 {
@@ -20,9 +22,35 @@ namespace podcastination_importer
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Btn_start_Click(object sender, RoutedEventArgs e)
+        {
+            // Create Object
+            object ImportTaskDetails = new
+            {
+                PodcastKey = TB_podcatsKey.Text,
+                SeasonKey = TB_seasonKey.Text,
+                Title = TB_title.Text,
+                Subtitle = TB_subtitle,
+                Date = DateTime.Now,
+                Author = TB_author,
+                Description = TB_description,
+                MP3FileName = TB_mp3FileName,
+                ImageFileName = TB_imageFileName,
+                YouTubeURL = TB_youTubeURL
+            };
+
+            // Transform ImportTaskDetails to Json Object
+            var jsonImportTaskDetails = JsonConvert.SerializeObject(ImportTaskDetails);
+            
+            Console.WriteLine(jsonImportTaskDetails);
         }
     }
 }
