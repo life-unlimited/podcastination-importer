bec48d9b64176f5251bde5cf0790ea32bd048d28
Nils Bezjak <6823416-nils_bezjak@users.noreply.gitlab.com>
2021-05-11 22:50:37 +0200

    fix: json gets serialized correctly now feat: added datepicker, added additional fields, changed date format

--- podcastination-importer/MainWindow.xaml.cs
+++ podcastination-importer/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.IO;
+using Microsoft.Win32;
 
 namespace podcastination_importer
 {
@@ -37,16 +38,19 @@ namespace podcastination_importer
             public string season_key { get; set; }
             public string title { get; set; }
             public string subtitle { get; set; }
-            public System.DateTime date { get; set; }
+            public DateTime date { get; set; }
             public string author { get; set; }
             public string description { get; set; }
-            public string mp3_file_name { get; set; }
-            public string image_file_name { get; set; }
+            public string mp3_file_location { get; set; }
+            public string image_file_location { get; set; }
+            public string pdf_file_location { get; set; }
             public string youtube_url { get; set; }
         }
 
         private void Btn_start_Click(object sender, RoutedEventArgs e)
         {
+            DateTime selectedDate = DateTimePicker.SelectedDate.Value;
+
             // Create Object
             ImportTaskDetails importTaskDetails = new ImportTaskDetails()
             {
@@ -54,26 +58,52 @@ namespace podcastination_importer
                 season_key = TB_seasonKey.Text,
                 title = TB_title.Text,
                 subtitle = TB_subtitle.Text,
-                date = DateTime.Now,
+                date = new DateTime(selectedDate.Year, selectedDate.Month, selectedDate.Day),
                 author = TB_author.Text,
                 description = TB_description.Text,
-                mp3_file_name = TB_mp3FileName.Text,
-                image_file_name = TB_imageFileName.Text,
+                mp3_file_location = TB_mp3FileLocation.Text,
+                image_file_location = TB_imageFileLocation.Text,
+                pdf_file_location = TB_pdfFileLocation.Text,
                 youtube_url = TB_youTubeURL.Text
             };
 
-            // Transform ImportTaskDetails to Json Object
-            string jsonImportTaskDetails = JsonConvert.SerializeObject(importTaskDetails);
-
-            createJsonFile(jsonImportTaskDetails, "../movie.json");
+            createJsonFile(importTaskDetails, "../task.json");
         }
 
-        public void createJsonFile(string jsonObject, string path)
+        public void createJsonFile(object jsonObject, string path)
         {
+            
             using (StreamWriter file = File.CreateText(path))
             {
-                JsonSerializer serializer = new JsonSerializer();
+                JsonSerializerSettings settings = new JsonSerializerSettings();
+                settings.Formatting = Formatting.Indented;
+
+                JsonSerializer serializer = JsonSerializer.Create(settings);
                 serializer.Serialize(file, jsonObject);
+            }
+            MessageBox.Show("Process sucessfull!");
+        }
+
+        private void Btn_Mp3File_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "mp3 files (*.mp3)|*.mp3";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            try
+            {
+                openFileDialog1.ShowDialog();
+
+                    string fileSelected = openFileDialog1.FileName;
+                    TB_mp3FileLocation.Text = fileSelected;
+                
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Something went wrong, while getting the file. Try agian");
             }
         }
     }
