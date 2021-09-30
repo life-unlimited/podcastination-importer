0e75a805b465283541914936f62315bab073d067
Nils Bezjak <6823416-nils_bezjak@users.noreply.gitlab.com>
2021-09-30 22:48:39 +0200

    feat: finish imoprter

--- podcastination-importer/MainWindow.xaml.cs
+++ podcastination-importer/MainWindow.xaml.cs
@@ -17,6 +17,9 @@ using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.IO;
 using Microsoft.Win32;
+using System.Configuration;
+using System.Globalization;
+using System.Collections;
 
 namespace podcastination_importer
 {
@@ -25,11 +28,15 @@ namespace podcastination_importer
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        const string Mp3OutputName = "sermon.mp3";
+        const string ThumbOutputName = "thumb.png";
+        const string PdfOutputName = "presentation.pdf";
 
         public MainWindow()
         {
             InitializeComponent();
+            DateTimePicker.SelectedDate = DateTime.Today;
+            refreshFormStates();
         }
 
         public class ImportTaskDetails
@@ -38,14 +45,14 @@ namespace podcastination_importer
             public string season_key { get; set; }
             public string title { get; set; }
             public string subtitle { get; set; }
-            public DateTime date { get; set; }
+            public string date { get; set; }
             public string time { get; set; }
             public string author { get; set; }
             public string description { get; set; }
-            public string mp3_file_location { get; set; }
-            public string image_file_location { get; set; }
-            public string pdf_file_location { get; set; }
-            public string youtube_url { get; set; }
+            public string mp3_file { get; set; }
+            public string image_file { get; set; }
+            public string pdf_file { get; set; }
+            public string yt_url { get; set; }
         }
 
         private void Btn_start_Click(object sender, RoutedEventArgs e)
@@ -54,254 +61,328 @@ namespace podcastination_importer
             string mp3Path = TB_mp3FileLocation.Text;
             string imagePath = TB_imageFileLocation.Text;
             string pdfPath = TB_pdfFileLocation.Text;
-            string jsonPath = "../task.json";
+            string jsonPath = ConfigurationManager.AppSettings["taskJsonPath"];
             DateTime selectedDate = DateTimePicker.SelectedDate.Value;
+            DateTime dateWithTime = addTimeToDate(selectedDate);
 
             // Create Object
             ImportTaskDetails importTaskDetails = new ImportTaskDetails()
             {
-                podcast_key = TB_podcatsKey.Text,
+                podcast_key = TB_podcastKey.Text,
                 season_key = TB_seasonKey.Text,
                 title = TB_title.Text,
                 subtitle = TB_subtitle.Text,
-                date = new DateTime(selectedDate.Year, selectedDate.Month, selectedDate.Day),
-                time = TB_Time.Text,
+                date = dateWithTime.ToString("yyyy-MM-dd'T'HH:mm:ss.ffZ"),
                 author = TB_author.Text,
                 description = TB_description.Text,
-                mp3_file_location = TB_mp3FileLocation.Text,
-                image_file_location = TB_imageFileLocation.Text,
-                pdf_file_location = TB_pdfFileLocation.Text,
-                youtube_url = TB_youTubeURL.Text
+                mp3_file = Mp3OutputName,
+                image_file = imagePath == "" ? "" : ThumbOutputName,
+                pdf_file = pdfPath == "" ? "" : PdfOutputName,
+                yt_url = TB_youTubeURL.Text
             };
 
             createJsonFile(importTaskDetails, jsonPath);
             moveFiles(mp3Path, imagePath, pdfPath, jsonPath);
-            MessageBox.Show("Success!");
         }
 
         public void createJsonFile(object jsonObject, string path)
         {
-            if(path == "")
+            try
             {
-                string directory = @"..\presets\";
-
-                // Path to directory before the instaltion directory
-                string combinedPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), directory);
-
-                SaveFileDialog saveFileDialog = new SaveFileDialog();
-                saveFileDialog.InitialDirectory = System.IO.Path.GetFullPath(combinedPath);
-                saveFileDialog.Filter = "json files (*.json)|*.json";
-                saveFileDialog.FilterIndex = 1;
-                saveFileDialog.RestoreDirectory = true;
-
-                if (saveFileDialog.ShowDialog() == true)
+                if (!Directory.Exists(path))
                 {
-                   path = saveFileDialog.FileName;
+                    Directory.CreateDirectory(path);
+                }
+                using (StreamWriter file = File.CreateText(path + @"\task.json"))
+                {
+                    JsonSerializerSettings settings = new JsonSerializerSettings();
+                    settings.Formatting = Formatting.Indented;
+
+                    JsonSerializer serializer = JsonSerializer.Create(settings);
+                    serializer.Serialize(file, jsonObject);
                 }
             }
-
-            using (StreamWriter file = File.CreateText(path))
+            catch (Exception e)
             {
-                JsonSerializerSettings settings = new JsonSerializerSettings();
-                settings.Formatting = Formatting.Indented;
+                MessageBox.Show(e.ToString());
+            };
 
-                JsonSerializer serializer = JsonSerializer.Create(settings);
-                serializer.Serialize(file, jsonObject);
+        }
+
+        public DateTime addTimeToDate(DateTime selectedDate)
+        {
+            DateTime dateWithTime;
+            string timeValue = TB_Time.Text;
+
+            string[] timeValueArray = timeValue.Split(":");
+            if (timeValueArray.Length != 2)
+            {
+                throw new Exception("Wrong time format. Expected: 'hh:mm'");
             }
+            selectedDate = selectedDate.AddHours(Convert.ToDouble(timeValueArray[0]));
+            selectedDate = selectedDate.AddMinutes(Convert.ToDouble(timeValueArray[1]));
+            dateWithTime = selectedDate;
+
+            return dateWithTime;
         }
 
         private void Btn_Mp3File_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "mp3 files (*.mp3)|*.mp3";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
+            openFileDialog.Filter = "mp3 files (*.mp3)|*.mp3";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
 
             try
             {
-                openFileDialog1.ShowDialog();
+                if ((bool)!openFileDialog.ShowDialog())
+                {
+                    return;
+                };
 
-                string fileSelected = openFileDialog1.FileName;
+                string fileSelected = openFileDialog.FileName;
                 TB_mp3FileLocation.Text = fileSelected;
 
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                MessageBox.Show($"Something went wrong: \n{ex}");
+                MessageBox.Show("Something went wrong, while getting the mp3. Try agian");
             }
         }
 
         private void Btn_ImageFile_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "png files (*.png)|*.png";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
+            openFileDialog.Filter = "png files (*.png)|*.png";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
 
             try
             {
-                openFileDialog1.ShowDialog();
+                if ((bool)!openFileDialog.ShowDialog())
+                {
+                    return;
+                };
 
-                string fileSelected = openFileDialog1.FileName;
+                string fileSelected = openFileDialog.FileName;
                 TB_imageFileLocation.Text = fileSelected;
 
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                MessageBox.Show($"Something went wrong: \n{ex}");
+                MessageBox.Show("Something went wrong, while getting the image. Try agian");
             }
         }
 
         private void Btn_PdfFile_Click(object sender, RoutedEventArgs e)
         {
-            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
-            openFileDialog1.InitialDirectory = "c:\\";
-            openFileDialog1.Filter = "pdf files (*.pdf)|*.pdf";
-            openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
+            openFileDialog.Filter = "pdf files (*.pdf)|*.pdf";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
 
             try
             {
-                openFileDialog1.ShowDialog();
+                if ((bool)!openFileDialog.ShowDialog())
+                {
+                    return;
+                };
 
-                string fileSelected = openFileDialog1.FileName;
+                string fileSelected = openFileDialog.FileName;
                 TB_pdfFileLocation.Text = fileSelected;
 
             }
-            catch (Exception ex)
+            catch (Exception)
             {
-                MessageBox.Show($"Something went wrong: \n{ex}");
+                MessageBox.Show("Something went wrong, while getting the pdf. Try agian");
             }
         }
 
         public void moveFiles(string mp3File, string imageFile, string pdfFile, string jsonPath)
         {
-            DateTime thisDay = DateTime.Today;
-            string thisDayConverted = thisDay.ToString("d");
-
+            DateTime thisDay = DateTime.Now;
+            string thisDayConverted = $"{thisDay.Year}-{thisDay.Month}-{thisDay.Day}-{thisDay.Hour}-{thisDay.Minute}-{thisDay.Second}";
+            string fullPath = System.IO.Path.Combine(ConfigurationManager.AppSettings["destination"] + thisDayConverted);
             try
             {
-                // files will get stored in a dircetory in the same path as the program is installed 
                 // The directory has the current date as a name
 
-                DirectoryInfo di = Directory.CreateDirectory(@$".\{thisDayConverted}");
+                DirectoryInfo di = Directory.CreateDirectory(fullPath);
 
-                File.Move(mp3File, di.FullName + @"\predigt.mp3");
+                File.Copy(mp3File, System.IO.Path.Join(di.FullName, Mp3OutputName));
 
                 if (imageFile != "")
                 {
-                    File.Move(imageFile, di.FullName + @"\thumb.png");
+                    File.Copy(imageFile, System.IO.Path.Join(di.FullName, ThumbOutputName));
                 }
 
                 if (pdfFile != "")
                 {
-                    File.Move(pdfFile, di.FullName + @"\pdfPredipt.pdf");
+                    File.Copy(pdfFile, System.IO.Path.Join(di.FullName, PdfOutputName));
                 }
 
-                File.Move(jsonPath, di.FullName + @"\task.json");
+                File.Move(jsonPath + @"\task.json", di.FullName + @"\task.json");
+
+                MessageBox.Show($"Files have been successfuly moved to {fullPath}");
+
+                System.Windows.Application.Current.Shutdown();
 
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                MessageBox.Show($"Something went wrong while moving the files: \n{ex}");
+                MessageBox.Show($"Something went wrong while moving the files: \n{e}");
             }
         }
 
         private void Btn_saveAsPreset_Click(object sender, RoutedEventArgs e)
         {
-            string directory = @"..\presets\";
-            string path = "";
-            if (!Directory.Exists(directory))
+            string jsonPath = String.Empty;
+            string filepath = ConfigurationManager.AppSettings["presetLocation"];
+            if (!Directory.Exists(filepath))
             {
-                Directory.CreateDirectory(directory);
+                Directory.CreateDirectory(filepath);
             }
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = filepath;
+            saveFileDialog.DefaultExt = "json";
+            saveFileDialog.AddExtension = true;
+            if ((bool)!saveFileDialog.ShowDialog())
+            {
+                return;
+            };
+            jsonPath = saveFileDialog.FileName;
+
 
             // Create Object
             ImportTaskDetails importTaskDetails = new ImportTaskDetails()
             {
-                podcast_key = TB_podcatsKey.Text,
+                podcast_key = TB_podcastKey.Text,
                 season_key = TB_seasonKey.Text,
                 title = TB_title.Text,
                 subtitle = TB_subtitle.Text,
                 time = TB_Time.Text,
                 author = TB_author.Text,
                 description = TB_description.Text,
-                mp3_file_location = TB_mp3FileLocation.Text,
-                image_file_location = TB_imageFileLocation.Text,
-                pdf_file_location = TB_pdfFileLocation.Text,
-                youtube_url = TB_youTubeURL.Text
+                mp3_file = TB_mp3FileLocation.Text,
+                image_file = TB_imageFileLocation.Text,
+                pdf_file = TB_pdfFileLocation.Text,
+                yt_url = TB_youTubeURL.Text
             };
 
-            createJsonFile(importTaskDetails, path);
+            createJsonFile(importTaskDetails, jsonPath);
         }
 
         private void Btn_openPreset_Click(object sender, RoutedEventArgs e)
         {
             string filePath;
-            string directory = @"..\presets\";
 
-            // Path to directory before the instaltion directory
-            string combinedPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), directory); 
-
+            string directory = ConfigurationManager.AppSettings["presetLocation"];
             if (!Directory.Exists(directory))
             {
-                MessageBox.Show("Es gibt noch keine Presets!");
+                Directory.CreateDirectory(directory);
             }
 
-            // Start file dialog
             OpenFileDialog openFileDialog = new OpenFileDialog();
             try
             {
-                openFileDialog.InitialDirectory = System.IO.Path.GetFullPath(combinedPath);
                 openFileDialog.Filter = "json files (*.json)|*.json";
                 openFileDialog.FilterIndex = 1;
                 openFileDialog.RestoreDirectory = true;
-                
-                if (openFileDialog.ShowDialog() == true)
+                if ((bool)!openFileDialog.ShowDialog())
                 {
+                    return;
+                };
 
-                    //Get the path of specified file
-                    filePath = openFileDialog.FileName;
+                //Get the path of specified file
+                filePath = openFileDialog.FileName;
 
-                    using (StreamReader file = File.OpenText(filePath))
-                    {
-                        JsonSerializer serializer = new JsonSerializer();
-                        ImportTaskDetails importTaskDetails = (ImportTaskDetails)serializer.Deserialize(file, typeof(ImportTaskDetails));
+                using (StreamReader file = File.OpenText(filePath))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    ImportTaskDetails importTaskDetails = (ImportTaskDetails)serializer.Deserialize(file, typeof(ImportTaskDetails));
 
-                        setPresetText(importTaskDetails);
-                    }
+                    setPresetText(importTaskDetails);
                 }
-
             }
             catch (Exception ex)
             {
-                MessageBox.Show($"Something went wrong: \n{ex}");
+                MessageBox.Show(Convert.ToString(ex));
             }
+
+
         }
 
         public void setPresetText(ImportTaskDetails importTaskDetails)
         {
-            TB_podcatsKey.Text = importTaskDetails.podcast_key;
+            TB_podcastKey.Text = importTaskDetails.podcast_key;
             TB_seasonKey.Text = importTaskDetails.season_key;
             TB_title.Text = importTaskDetails.title;
             TB_Time.Text = importTaskDetails.time;
             TB_author.Text = importTaskDetails.author;
             TB_description.Text = importTaskDetails.description;
-            TB_mp3FileLocation.Text = importTaskDetails.mp3_file_location;
-            TB_imageFileLocation.Text = importTaskDetails.image_file_location;
-            TB_pdfFileLocation.Text = importTaskDetails.pdf_file_location;
-            TB_youTubeURL.Text = importTaskDetails.youtube_url;
+            TB_mp3FileLocation.Text = importTaskDetails.mp3_file;
+            TB_imageFileLocation.Text = importTaskDetails.image_file;
+            TB_pdfFileLocation.Text = importTaskDetails.pdf_file;
+            TB_youTubeURL.Text = importTaskDetails.yt_url;
         }
 
-        private void TB_mp3FileLocation_TextChanged(object sender, TextChangedEventArgs e)
+        private List<string> validateForm()
         {
+            List<string> errorList = new List<string>();
 
-            Btn_start.IsEnabled = TB_mp3FileLocation.Text != "";
+            if (TB_podcastKey.Text == "")
+                errorList.Add("Missing PodcastKey.");
+
+            if (TB_seasonKey.Text == "")
+                errorList.Add("Missing SeasonKey.");
+
+            if (TB_title.Text == "")
+                errorList.Add("Missing Title.");
+
+            if (TB_Time.Text == "")
+                errorList.Add("Missing Time.");
+
+            if (TB_author.Text == "")
+                errorList.Add("Missing Author.");
+
+            if (TB_mp3FileLocation.Text == "")
+                errorList.Add("Missing MP3 file.");
+
+            if (DateTimePicker.SelectedDate == null)
+                errorList.Add("Missing Date.");
+
+            return errorList;
+        }
+
+        public void refreshFormStates()
+        {
+            List<string> errorList = validateForm();
+            if(errorList.Count == 0)
+            {
+                TB_validStates.Text = "No errors.";
+                TB_validStates.Foreground = Brushes.White;
+                Btn_start.IsEnabled = true;
+            }
+            else
+            {
+                TB_validStates.Text = String.Join("\n", errorList);
+                TB_validStates.Foreground = Brushes.Red;
+                Btn_start.IsEnabled = false;
+            }
+        }
+
+        private void refreshFormStates(object sender, TextChangedEventArgs e)
+        {
+            refreshFormStates();
+        }
+
+        private void refreshFormStates(object sender, SelectionChangedEventArgs e)
+        {
+            refreshFormStates();
         }
     }
 }
